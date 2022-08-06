83371563e68a90c51a288c512542bfed2e9192be
Pablo Santos <binho.bgu@gmail.com>
2022-08-06 15:48:25 -0300

    Refatorando contagem de animais para classe

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,24 +1,31 @@
-import initNumetos from "./cont-numeros.js";
+import AnimaNumeros from "./cont-numeros.js";
 
 export default function initFetchAnimais() {
-  async function fetchAnimais(url) {
-    const animaisResponse = await fetch(url);
-    const animaisJson = await animaisResponse.json();
-    const numerosGrid = document.querySelector(".numeros-grid");
-    animaisJson.forEach((animal) => {
-      const divAnimal = createAnimal(animal);
-      numerosGrid.appendChild(divAnimal);
-    });
-    initNumetos();
-  }
-
   function createAnimal(animal) {
     const div = document.createElement("div");
     div.classList.add("numero-animal");
-
-    div.innerHTML = `<h3>${animal.specie}</h3> <span data-numero>${animal.total}</span>`;
-
+    div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
     return div;
+  }
+
+  async function fetchAnimais(url) {
+    try {
+      const animaisResponse = await fetch(url);
+      const animaisJSON = await animaisResponse.json();
+      const numerosGrid = document.querySelector(".numeros-grid");
+      animaisJSON.forEach((animal) => {
+        const divAnimal = createAnimal(animal);
+        numerosGrid.appendChild(divAnimal);
+      });
+      const animaNumeros = new AnimaNumeros(
+        "[data-numero]",
+        ".numeros",
+        "ativo"
+      );
+      animaNumeros.init();
+    } catch (erro) {
+      console.log(erro);
+    }
   }
 
   fetchAnimais("json/animaisapi.json");
